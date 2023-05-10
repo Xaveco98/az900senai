c77436c02999a3bc681e11be47abe619a756b7d0
Xaveco98 <133116572+Xaveco98@users.noreply.github.com>
2023-05-10 11:00:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="custom.css">
 </head>
 <body>
-  <h1 style="color: red">Minha página inicial</h1>
+  <h1>Minha página inicial</h1>
   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br>
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br>
     when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
